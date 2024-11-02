cdac9096c56e8a310cb17f7417aafb9df3d83e34
Siddhi Sahu <himanisahu739@gmail.com>
2024-11-02 12:53:46 +0530

    backend init

---
+++ src/store/Store.ts
@@ -0,0 +1,25 @@
+type UserId = string;
+
+interface Chat {
+    userId: UserId;
+    name: string;
+    message: string;
+    upvotes: UserId[]; //who has upvotes what
+}
+
+export abstract class Store {
+    constructor() {
+
+    }
+    initRoom() {
+
+    }
+    getChats(room: string, limit: number, offset: number) { }
+
+    addChat(room: string, limit: number, offset: number) {
+
+    }
+    upvote(room: string, chatId: string) {
+
+    }
+}
