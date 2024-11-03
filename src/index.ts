5ef9ddbb71b96d22dc2e673cb00fdc34cfeb044d
Siddhi Sahu <himanisahu739@gmail.com>
2024-11-03 18:02:52 +0530

    comit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 
 import { server as WebSocketServer } from "websocket"
 import http from 'http';
-var server = http.createServer(function (request: any, response: any) {
+const server = http.createServer(function (request: any, response: any) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
@@ -11,15 +11,11 @@ server.listen(8080, function () {
 });
 const wsServer = new WebSocketServer({
     httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
+
     autoAcceptConnections: false
 });
 function originIsAllowed(origin: string) {
-    // put logic here to detect whether the specified origin is allowed.
+
     return true;
 }
 wsServer.on('request', function (request) {
@@ -27,6 +23,7 @@ wsServer.on('request', function (request) {
         // Make sure we only accept requests from an allowed origin
         request.reject();
         console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+
         return;
     }
 
@@ -34,15 +31,23 @@ wsServer.on('request', function (request) {
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function (message) {
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
+            try {
+                messageHandler(JSON.parse(message.utf8Data))
+
+            } catch (e) {
+
+            }
+
+            // console.log('Received Message: ' + message.utf8Data);
+            // connection.sendUTF(message.utf8Data);
         }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
+
     });
     connection.on('close', function (reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+
+function messageHandler(message) {
+
+}
--- src/store/inMemoryStore.ts
+++ src/store/inMemoryStore.ts
@@ -1,4 +1,4 @@
-import { Chat, Store, UserId } from "./store/Store";
+import { Chat, Store, UserId } from "./Store";
 let globalChatId = 0;
 
 interface Room {
